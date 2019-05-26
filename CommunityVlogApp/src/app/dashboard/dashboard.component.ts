6b879579e9c55ff4e2be4f23c7aa2b7c2e0407cb
Akshay-Borkar <borkar.akshay@gmail.com>
2019-05-26 13:30:01 +0530

    API Connection

---
+++ CommunityVlogApp/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  values: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  getValues() {
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+      this.values = JSON.parse(JSON.stringify(response));
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+
+  }
+
+}

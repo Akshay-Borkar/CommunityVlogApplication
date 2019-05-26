0bbb1b68aa6090a4dee6e39ef53da354a125263b
Akshay-Borkar <borkar.akshay@gmail.com>
2019-05-26 22:10:25 +0530

    Navigation bar added

--- CommunityVlogApp/src/app/app.module.ts
+++ CommunityVlogApp/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      DashboardComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
